9c36abdf1dc20961e8758804b3bcecf7ced90ddc
MundingAkbar <sl201501058@wmsu.edu.ph>
2020-10-12 13:22:30 +0800

    Scaffolding the app with image

--- HelloWorld/lib/main.dart
+++ HelloWorld/lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Center(
-    child: Text('Hello World!'),
-  )));
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Hello World App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image(image: AssetImage('assets/image/world.jpg')),
+      ),
+    );
+  }
 }
