0ad31b5cb4fb962e2254fd51558cefa937082eca
MundingAkbar <sl201501058@wmsu.edu.ph>
2020-10-25 17:23:57 +0800

    icon changed and some other UI

--- HelloWorld/lib/main.dart
+++ HelloWorld/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(primaryColor: Colors.red),
+      debugShowCheckedModeBanner: false,
+      title: "chat UI",
       home: MyApp(),
     ),
   );
@@ -12,10 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text('Hello World App'),
+        leading: Icon(Icons.menu),
+        title: Text('Chat'),
         centerTitle: true,
+        elevation: 0.0,
+        actions: [Icon(Icons.search)],
       ),
       body: Center(
         child: Image(image: AssetImage('assets/image/world.jpg')),
